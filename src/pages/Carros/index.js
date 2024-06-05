c9aeade58de89ea6649eda9a31037d5e79dbca89
walther <waltheroliveirapires@gmail.com>
2024-06-05 02:45:36 -0300

    Adicionei lista de veículos baseada no backend

--- src/pages/Carros/index.js
+++ src/pages/Carros/index.js
@@ -1,6 +1,7 @@
 import React from "react";
+
 import Titulo from "../../components/Titulo";
-import Quadro from "../../components/Quadro";
+import Swr from "../../components/SWR";
 
 export default function Carros() {
   return (
@@ -9,9 +10,7 @@ export default function Carros() {
         {/*lado esquerdo*/}
         <div className="flex flex-col justify-between break-after-column mx-auto text-center pt-1 bg-neutral-700 min-w-[550px] min-h-full rounded">
           <Titulo className="w-full mx-auto">Carros</Titulo>
-          <Quadro className="my-1 mx-auto">
-            <p>Marca: </p>
-          </Quadro>
+          <Swr />
         </div>
 
         {/*lado direito*/}
